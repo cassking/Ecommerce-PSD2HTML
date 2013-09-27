c25a679cd64b38e03fca088e56a59400e581e7e3
Cassandra King <cking@Administrators-MacBook-Pro-7.local>
2013-09-27 16:01:58 -0400

    begin styling dropdown menus,  swapped out .clearfix for one w/o overflow:hidden to prevent clipping of content child elem

---
+++ site/js/jquery.aside-menu.js
@@ -0,0 +1,14 @@
+function mainmenu(){
+$(" ul.link-list li ul li ").hover(function(){
+		$(this).find('ul:first').css({visibility: "visible",display: "none"}).show(400);
+		},function(){
+		$(this).find('ul:first').css({visibility: "hidden"});
+		});
+}
+
+ 
+ 
+ $(document).ready(function(){					
+	mainmenu();
+});
+ 
